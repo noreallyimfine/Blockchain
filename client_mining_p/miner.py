19684069270b46df29bcf2acab92820ec4e24228
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-11 18:50:34 -0400

    ran through a proof and sent to server, still have bug in server response

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,14 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block)
+
+    proof = 0
+
+    while not valid_proof(block_string, proof):
+        proof += 1
+
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +34,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == '000000'
 
 
 if __name__ == '__main__':
@@ -56,14 +65,16 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
-        print(data)
+        print("Mining...")
+        block = data['last_block']
+        new_proof = proof_of_work(block)
+        print(new_proof)
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
-
+        print(data)
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
